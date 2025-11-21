6f3f8b14e9678d9e79e682037f7c3eb3950217fb
Alexander Vining <alexandervining1@gmail.com>
2025-11-21 20:53:57 +0100

    export

--- projective_simulation/envs/factorized_POMDPs.py
+++ projective_simulation/envs/factorized_POMDPs.py
@@ -412,6 +412,7 @@ class List_Sequencer(Factorized_POMDP):
                       stimulus_duration: int,
                       mean_timeout_interval: float = 4.0,
                       var_time_out_interval: float = 2.0,
+                      reuse_items: bool = True,
                       num_series: int = 3,
                       master_seed: Optional[int] = None,  # seed that deterministically produces per-sim child seeds
                       return_instances: bool = False,     # if False, return records with constructor_kwargs (+ arrays)
@@ -437,6 +438,7 @@ class List_Sequencer(Factorized_POMDP):
                         mean_timeout_interval=mean_timeout_interval,
                         var_time_out_interval=var_time_out_interval,
                         num_series=num_series,
+                        reuse_items=reuse_items,
                         seed=s)
                 )
             else:
@@ -447,6 +449,7 @@ class List_Sequencer(Factorized_POMDP):
                            mean_timeout_interval=mean_timeout_interval,
                            var_time_out_interval=var_time_out_interval,
                            num_series=num_series,
+                           reuse_items=reuse_items,
                            seed=s)
                 out.append(inst.to_record(include_arrays=include_arrays))
         return out
